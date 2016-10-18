e74ee33c90245a929dc50ff17c3d03213ac7639d
wouzar <wouzar@gmail.com>
2016-10-18 17:05:53 +0300

    Add status log

--- proxy/views.py
+++ proxy/views.py
@@ -5,10 +5,11 @@ from django.http import HttpResponse
 def index(request):
     url = request.GET.get('url')
     proxy = request.GET.get('proxy')
-    print(url)
-    print(proxy)
+    print('url:', url)
+    print('proxy:', proxy)
     proxies = { "http" : proxy }
     r = requests.get(url, proxies = proxies)
+    print('status:', r.status_code)
     text = r.text.encode('utf-8')
     return HttpResponse(text)
 
